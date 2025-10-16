6ee3b98af4a3eb057dec973e1deb5102d124681f
Tristan Phease <tristan.phease@gmail.com>
2025-10-16 19:51:16 +1300

    Add extra layer of buttons to gui

--- src/gui/gui_menu.rs
+++ src/gui/gui_menu.rs
@@ -2,7 +2,10 @@
 
 use bevy::prelude::*;
 
-use crate::{gui::ButtonMenuComponent, AppState};
+use crate::{
+    gui::{create_button, ButtonMenuComponent, GuiMenuData},
+    AppState,
+};
 
 #[derive(Component, Clone, Copy)]
 pub(super) struct GuiMenu;
@@ -28,6 +31,13 @@ pub(super) fn gui_menu_system(
     }
 }
 
+#[derive(Clone, Copy, Default, Debug, PartialEq, Eq, Hash, States)]
+pub(super) enum GuiMenuState {
+    #[default]
+    Empty,
+    Debug,
+}
+
 // for opening debug related things
 #[derive(Component, Clone, Copy)]
 pub(super) struct DebugMenu;
@@ -36,4 +46,60 @@ impl ButtonMenuComponent for DebugMenu {
     fn to_str(&self) -> &str {
         "Debug"
     }
+}
+
+pub(super) fn debug_menu_system(
+    interaction_query: Query<&Interaction, (With<DebugMenu>, Changed<Interaction>)>,
+    current_state: Res<State<GuiMenuState>>,
+    mut next_state: ResMut<NextState<GuiMenuState>>,
+) {
+    for interaction in interaction_query {
+        match interaction {
+            Interaction::Pressed => {
+                let new_state = if *current_state.get() == GuiMenuState::Debug {
+                    GuiMenuState::Empty
+                } else {
+                    GuiMenuState::Debug
+                };
+                next_state.set(new_state);
+            }
+            _ => {}
+        }
+    }
+}
+
+// for opening debug related things
+#[derive(Component, Clone, Copy)]
+pub(super) struct SaveImageButton;
+
+impl ButtonMenuComponent for SaveImageButton {
+    fn to_str(&self) -> &str {
+        "Save Image"
+    }
+}
+
+#[derive(Resource)]
+pub(super) struct DebugMenuData {
+    debug_menu_entity: Entity,
+}
+
+pub(super) fn setup_debug_menu(mut commands: Commands, gui_menu_data: Res<GuiMenuData>) {
+    //
+    let debug_menu_entity = commands
+        .spawn((
+            Node {
+                flex_direction: FlexDirection::Column,
+                ..default()
+            },
+            children![create_button(SaveImageButton)],
+        ))
+        .id();
+    commands
+        .entity(gui_menu_data.gui_menu_entity)
+        .add_child(debug_menu_entity);
+    commands.insert_resource(DebugMenuData { debug_menu_entity });
+}
+
+pub(super) fn close_debug_menu(mut commands: Commands, debug_menu_data: Res<DebugMenuData>) {
+    commands.entity(debug_menu_data.debug_menu_entity).despawn();
 }
--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -3,7 +3,10 @@ use bevy::{input_focus::InputFocus, prelude::*};
 use crate::{
     gui::{
         button::{button_system, create_button},
-        gui_menu::{gui_menu_system, DebugMenu, GuiMenu},
+        gui_menu::{
+            close_debug_menu, debug_menu_system, gui_menu_system, setup_debug_menu, DebugMenu,
+            GuiMenu, GuiMenuState,
+        },
         main_menu::{close_main_menu, setup_main_menu, start_button_menu_system},
     },
     AppState,
@@ -17,17 +20,29 @@ pub struct GuiPlugin;
 
 impl Plugin for GuiPlugin {
     fn build(&self, app: &mut App) {
+        // general stuff
         app.init_resource::<InputFocus>();
-        app.add_systems(Startup, setup_gui);
+        app.add_systems(Update, button_system);
+        // main menu stuff
         app.add_systems(OnEnter(AppState::Menu), setup_main_menu);
         app.add_systems(
             Update,
             start_button_menu_system.run_if(in_state(AppState::Menu)),
         );
         app.add_systems(OnExit(AppState::Menu), close_main_menu);
-        app.add_systems(Update, button_system);
+        // gui menu stuff
+        app.init_state::<GuiMenuState>();
+        app.add_systems(OnEnter(GuiMenuState::Debug), setup_debug_menu);
+        app.add_systems(OnExit(GuiMenuState::Debug), close_debug_menu);
+        app.add_systems(Startup, setup_gui);
         app.add_systems(Update, gui_menu_system);
+        app.add_systems(Update, debug_menu_system);
     }
+}
+
+#[derive(Resource)]
+pub(super) struct GuiMenuData {
+    gui_menu_entity: Entity,
 }
 
 trait ButtonMenuComponent: Component + Copy {
@@ -35,16 +50,25 @@ trait ButtonMenuComponent: Component + Copy {
 }
 
 pub(super) fn setup_gui(mut commands: Commands) {
-    commands.spawn((
-        Node {
-            width: percent(100),
-            height: percent(100),
-            align_items: AlignItems::FlexEnd,
-            justify_content: JustifyContent::FlexStart,
-            flex_direction: FlexDirection::Column,
-            padding: UiRect::all(px(20)),
-            ..default()
-        },
-        children![create_button(GuiMenu), create_button(DebugMenu)],
-    ));
+    let gui_menu_entity = commands
+        .spawn((
+            Node {
+                width: percent(100),
+                height: percent(100),
+                align_items: AlignItems::FlexStart,
+                justify_content: JustifyContent::FlexStart,
+                flex_direction: FlexDirection::RowReverse,
+                padding: UiRect::all(px(20)),
+                ..default()
+            },
+            children![(
+                Node {
+                    flex_direction: FlexDirection::Column,
+                    ..default()
+                },
+                children![create_button(GuiMenu), create_button(DebugMenu)]
+            )],
+        ))
+        .id();
+    commands.insert_resource(GuiMenuData { gui_menu_entity });
 }
